dc242fd939abe1e487c3219d2a9781b10e34e26a
Parag Varshney <lit2018063@iiitl.ac.in>
2021-05-24 13:01:17 +0530

    Update mongodb.service.js

--- app/services/mongodb.service.js
+++ app/services/mongodb.service.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 exports.connect = () => {
     // connecting to mongo database
-    mongoose.connect('mongodb://localhost:27017/placementiiitl', { useNewUrlParser: true, useFindAndModify: false ,useUnifiedTopology: true }, function (err) {
+    mongoose.connect('process.env.MONGO_URI', { useNewUrlParser: true, useFindAndModify: false ,useUnifiedTopology: true }, function (err) {
         if(err) {
             console.log(err);
         } else {
