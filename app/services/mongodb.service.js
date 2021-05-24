57f6a1bdef1fbdd8160712e227a5f18b9fcc6d2a
Parag Varshney <lit2018063@iiitl.ac.in>
2021-05-24 16:25:47 +0530

    Update mongodb.service.js

--- app/services/mongodb.service.js
+++ app/services/mongodb.service.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 exports.connect = () => {
     // connecting to mongo database
-    mongoose.connect('process.env.MONGO_URI', { useNewUrlParser: true, useFindAndModify: false ,useUnifiedTopology: true }, function (err) {
+    mongoose.connect('process.env.MONGO_CONNECTION_URI', { useNewUrlParser: true, useFindAndModify: false ,useUnifiedTopology: true }, function (err) {
         if(err) {
             console.log(err);
         } else {
